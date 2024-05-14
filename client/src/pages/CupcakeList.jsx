00c732db45d636882b8dcad888af9db11dff5b83
Loic legoff <loic95280@gmail.com>
2024-05-14 12:52:37 +0200

    step5

--- client/src/pages/CupcakeList.jsx
+++ client/src/pages/CupcakeList.jsx
@@ -44,13 +44,13 @@ function CupcakeList() {
   console.info(useLoaderData());
 
   // Step 3: get all accessories
-  const [accessories, setAccesories] = useState([]);
+  const [accessories, setAccessories] = useState([]);
 
   function getAccessories() {
     fetch("http://localhost:3310/api/accessories")
       .then((response) => response.json())
       .then((data) => {
-        setAccesories(data);
+        setAccessories(data);
       });
   }
   useEffect(() => {
@@ -59,34 +59,50 @@ function CupcakeList() {
   console.info(accessories);
 
   // Step 5: create filter state
+  const [selectedAccess, setSelectedAccess] = useState("");
 
+  function handleChange(event) {
+    setSelectedAccess(event.target.value);
+  }
   return (
     <>
       <h1>My cupcakes</h1>
       <form className="center">
         <label htmlFor="cupcake-select">
           {/* Step 5: use a controlled component for select */}
-          Filter by{" "}
-            {/* Step 4: add an option for each accessory */}
-          <select id="cupcake-select">
-              <option value="">---</option>
-              <option value="1">Cherry</option>
-              <option value="2">Donut</option>
-              <option value="3">Chocolate</option>
-              <option value="4">Wild</option>
-              <option value="5">Christmas Candy</option>
-            </select>
+          Filter by {/* Step 4: add an option for each accessory */}
+          <select
+            id="cupcake-select"
+            onChange={handleChange}
+            value={selectedAccess}
+          >
+            <option value="">---</option>
+            <option value="1">Cherry</option>
+            <option value="2">Donut</option>
+            <option value="3">Chocolate</option>
+            <option value="4">Wild</option>
+            <option value="5">Christmas Candy</option>
+          </select>
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
-        {cupcakesData.map((cupcake) => (
-          <li key={cupcake.id}>
-            <Cupcake data={cupcake} />
+        {cupcakesData
+          .filter(
+            (cupcake) =>
+              selectedAccess === "" || selectedAccess === cupcake.accessory_id
+          )
+          .map((cupcake) => (
+            <li className="cupcake-item" key={cupcake.id}>
+              <Cupcake cupcake={cupcake} data={cupcake} />
+            </li>
+          ))}
+        {/* Step 5: filter cupcakes before repeating */}
+        {accessories.map((accessory) => (
+          <li className="cupcake-item" key={accessory.id}>
+            <Cupcake accessory={accessory} data={accessory} />
           </li>
         ))}
-        {/* Step 5: filter cupcakes before repeating */}
-        {/* end of block */}
       </ul>
     </>
   );
