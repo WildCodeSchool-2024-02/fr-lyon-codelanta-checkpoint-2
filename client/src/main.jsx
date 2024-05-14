4e426576c1b8d60524e1cb611bbcffe73f7dde9a
YohanGH <YohanGH@proton.me>
2024-05-14 10:21:42 +0200

    Finish step 1 and create map step 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,40 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import App from "./App";
-
 import Home from "./pages/Home";
 import Instructions from "./pages/Instructions";
 import CupcakeList from "./pages/CupcakeList";
 
+// Fonction des cupcakes ici
+async function fetchCupcakes() {
+	const response = await fetch("http://localhost:3310/api/cupcakes");
+	if (!response.ok) {
+		throw new Error("Failed to fetch cupcakes");
+	}
+	return response.json();
+}
+
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/instructions",
-        element: <Instructions />,
-      },
-      {
-        path: "/cupcakes",
-        element: <CupcakeList />,
-        // Step 1: load data here
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/instructions",
+				element: <Instructions />,
+			},
+			{
+				path: "/cupcakes",
+				element: <CupcakeList />,
+				// Step 1: load data here
+				loader: fetchCupcakes,
+			},
+		],
+	},
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
