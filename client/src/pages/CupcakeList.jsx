707b83a26490ea28d885dc0f1c5009fb0c56c7ac
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-05-14 11:12:32 +0200

    step3

--- client/src/pages/CupcakeList.jsx
+++ client/src/pages/CupcakeList.jsx
@@ -1,46 +1,66 @@
 import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Cupcake from "../components/Cupcake";
 
 /* ************************************************************************* */
-const someCupcakes = [];
-someCupcakes.push(
-  {
-    id: 10,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "blue",
-    color2: "white",
-    color3: "red",
-    name: "France",
-  },
-  {
-    id: 11,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "yellow",
-    color2: "red",
-    color3: "black",
-    name: "Germany",
-  },
-  {
-    id: 27,
-    accessory_id: "5",
-    accessory: "christmas-candy",
-    color1: "yellow",
-    color2: "blue",
-    color3: "blue",
-    name: "Sweden",
-  }
-);
+// const someCupcakes = [];
+// someCupcakes.push(
+//   {
+//     id: 10,
+//     accessory_id: "4",
+//     accessory: "wcs",
+//     color1: "blue",
+//     color2: "white",
+//     color3: "red",
+//     name: "France",
+//   },
+//   {
+//     id: 11,
+//     accessory_id: "4",
+//     accessory: "wcs",
+//     color1: "yellow",
+//     color2: "red",
+//     color3: "black",
+//     name: "Germany",
+//   },
+//   {
+//     id: 27,
+//     accessory_id: "5",
+//     accessory: "christmas-candy",
+//     color1: "yellow",
+//     color2: "blue",
+//     color3: "blue",
+//     name: "Sweden",
+//   }
+// );
 
 /* you can use someCupcakes if you're stucked on step 1 */
 /* if you're fine with step 1, just ignore this ;) */
 /* ************************************************************************* */
 
 function CupcakeList() {
-  console.info(useLoaderData());
+  const [cupcakelist, setCupcakeList] = useState([]);
+  const [cupccupcakeAccessories, setCupcakeAccessories] = useState([]);
 
-  // Step 3: get all accessories
+  function getCupcakeAccessories() {
+    axios
+      .get("http://localhost:3310/api/accessories")
+      .then((response) => setCupcakeAccessories(response.data));
+  }
+
+  function getCupcake() {
+    axios
+      .get("http://localhost:3310/api/cupcakes")
+      .then((response) => setCupcakeList(response.data));
+  }
+
+  useEffect(() => {
+    getCupcakeAccessories();
+    getCupcake();
+  }, []);
+
+  console.info(useLoaderData());
 
   // Step 5: create filter state
 
@@ -49,7 +69,11 @@ function CupcakeList() {
       <h1>My cupcakes</h1>
       <form className="center">
         <label htmlFor="cupcake-select">
-          {/* Step 5: use a controlled component for select */}
+          {cupccupcakeAccessories.map((el) => (
+            <option key={el.id} value={el.id}>
+              {el.name}
+            </option>
+          ))}
           Filter by{" "}
           <select id="cupcake-select">
             <option value="">---</option>
@@ -61,7 +85,7 @@ function CupcakeList() {
         {/* Step 2: repeat this block for each cupcake */}
         {/* Step 5: filter cupcakes before repeating */}
         <li className="cupcake-item">
-          {someCupcakes.map((cupcake) => (
+          {cupcakelist.map((cupcake) => (
             <li key={cupcake.id} className="cupcake-item">
               <Cupcake data={cupcake} />
             </li>
