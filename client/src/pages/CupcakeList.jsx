f3183eee03b2fb1bd2732829dfa4c4b629147487
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-05-14 11:09:27 +0200

    step 2 done

--- client/src/pages/CupcakeList.jsx
+++ client/src/pages/CupcakeList.jsx
@@ -60,10 +60,12 @@ function CupcakeList() {
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
-        {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake />
-        </li>
+        {someCupcakes.map((cupcake) => (
+          <li key={cupcake.id} className="cupcake-item">
+            <Cupcake cupcake={cupcake} />
+            {/* Step 5: filter cupcakes before repeating */}
+          </li>
+        ))}
         {/* end of block */}
       </ul>
     </>
